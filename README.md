d114ece163dc1f3101fab1c08fb97ec4665514ad
sai <1430792918@qq.com>
2025-07-24 16:24:45 +0800

    first

---
+++ README.md
@@ -0,0 +1,29 @@
+<p align="center">
+  <img src="https://saithink.top/images/logo.png" width="120" />
+</p>
+<p align="center">
+  <img src="https://svg.hamm.cn/badge.svg?key=License&value=MIT" />
+  <img src="https://svg.hamm.cn/badge.svg?key=Version&value=5.x" />
+</p>
+
+<div style="padding:18px;max-width: 1024px;margin:0 auto;">
+<h1>saipackage 1.x</h1>
+
+基于<a href="https://saithink.top" target="_blank">saiadmin</a>创建的插件安装器，能够方便的管理安装`saiadmin`插件
+
+<h1>安装</h1>
+
+composer环境的安装命令如下
+
+```bash
+composer require saithink/saipackage
+```
+
+本插件必须在`saiadmin`环境下才能使用，请先配置`saiadmin`
+
+<div style="clear: both">
+<h1>LICENSE</h1>
+This project is open-sourced software licensed under the MIT.
+</div>
+
+</div>
---
+++ src/plugin/saipackage/app/controller/InstallController.php
@@ -0,0 +1,160 @@
+<?php
+
+namespace plugin\saipackage\app\controller;
+
+use plugin\saiadmin\app\middleware\SystemLog;
+use plugin\saiadmin\app\middleware\CheckLogin;
+use plugin\saiadmin\basic\BaseController;
+use plugin\saiadmin\exception\ApiException;
+use plugin\saipackage\app\logic\InstallLogic;
+use Saithink\Saipackage\service\Server;
+use Saithink\Saipackage\service\Version;
+use support\annotation\Middleware;
+use support\Request;
+use support\Response;
+use Throwable;
+
+#[Middleware(CheckLogin::class, SystemLog::class)]
+class InstallController extends BaseController
+{
+    /**
+     * 构造
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        if ($this->adminId > 1) {
+            throw new ApiException('仅超级管理员能够操作');
+        }
+    }
+
+    /**
+     * 环境检查状态
+     */
+    static string $ok   = 'ok';
+    static string $fail = 'fail';
+    static string $warn = 'warn';
+
+    static array $needDependentVersion = [
+        'php'  => '8.1.0',
+        'saiadmin'  => '5.1.0',
+        'saithink' => '1.0.0',
+    ];
+
+    /**
+     * 应用列表
+     * @param Request $request
+     * @return Response
+     */
+    public function index(Request $request): Response
+    {
+        $data = Server::installedList(runtime_path() . DIRECTORY_SEPARATOR . 'saithink' . DIRECTORY_SEPARATOR);
+
+        $phpVersion        = phpversion();
+        $phpVersionCompare = Version::compare(self::$needDependentVersion['php'], $phpVersion);
+        $phpVersionNotes = '正常';
+        if (!$phpVersionCompare) {
+            $phpVersionNotes = '需要版本' . ' >= ' . self::$needDependentVersion['php'];
+        }
+
+        $saiadminVersion        = config('plugin.saiadmin.app.version');
+        $saiadminVersionCompare = Version::compare(self::$needDependentVersion['saiadmin'], $saiadminVersion);
+        $saiadminVersionNotes = '正常';
+        if (!$saiadminVersionCompare) {
+            $saiadminVersionNotes = '需要版本' . ' >= ' . self::$needDependentVersion['saiadmin'];
+        }
+
+        $saithinkVersion        = config('plugin.saithink.app.version');
+        $saithinkVersionCompare = Version::compare(self::$needDependentVersion['saithink'], $saithinkVersion);
+        $saithinkVersionNotes = '正常';
+        if (!$saithinkVersionCompare) {
+            $saithinkVersionNotes = '需要版本' . ' >= ' . self::$needDependentVersion['saithink'];
+        }
+
+
+        return $this->success([
+            'version' => [
+                'php_version' => [
+                    'describe' => $phpVersion,
+                    'state'  => $phpVersionCompare ? self::$ok : self::$fail,
+                    'notes'   => $phpVersionNotes,
+                ],
+                'saiadmin_version' => [
+                    'describe' => $saiadminVersion,
+                    'state'  => $saiadminVersionCompare ? self::$ok : self::$fail,
+                    'notes'   => $saiadminVersionNotes,
+                ],
+                'saithink_version' => [
+                    'describe' => $saithinkVersion,
+                    'state'  => $saithinkVersionCompare ? self::$ok : self::$fail,
+                    'notes'   => $saithinkVersionNotes,
+                ],
+            ],
+            'data' => $data
+        ]);
+    }
+
+    /**
+     * 上传插件
+     * @param Request $request
+     * @return Response
+     * @throws Throwable
+     */
+    public function upload(Request $request): Response
+    {
+        $file  = $request->post("file", '');
+        if (empty($file)) {
+            return $this->fail('参数错误');
+        }
+        $install = new InstallLogic();
+        $info = $install->upload($file);
+        return $this->success($info);
+    }
+
+    /**
+     * 安装插件
+     * @param Request $request
+     * @return Response
+     * @throws Throwable
+     */
+    public function install(Request $request): Response
+    {
+        $appName  = $request->post("appName", '');
+        if (empty($appName)) {
+            return $this->fail('参数错误');
+        }
+        $install = new InstallLogic($appName);
+        $info = $install->install();
+        return $this->success($info);
+    }
+
+    /**
+     * 卸载插件
+     * @param Request $request
+     * @return Response
+     * @throws Throwable
+     */
+    public function uninstall(Request $request): Response
+    {
+        $appName  = $request->post("appName", '');
+        if (empty($appName)) {
+            return $this->fail('参数错误');
+        }
+        $install = new InstallLogic($appName);
+        $install->uninstall();
+        return $this->success('卸载插件成功');
+    }
+
+    /**
+     * 重启
+     * @param Request $request
+     * @return Response
+     */
+    public function reload(Request $request): Response
+    {
+        Server::restart();
+
+        return $this->success('重载成功');
+    }
+
+}
